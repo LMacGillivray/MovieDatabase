19b38e5cb1c2c24db06aa9b3a1c27c79d8cee5cf
lucky-mac11 <luckyjaguar@gmail.com>
2017-10-07 11:27:29 -0400

    Changed LibrayViewer to LibraryController
    Added comments to Library Controller
    
    Simplified responses variable, removed keywords
    
    Updated Add option

---
+++ MovieDatabase/src/Controllers/LibraryController.java
@@ -0,0 +1,125 @@
+/* MovieDatabase.Controllers.MovieLibrary
+ * © lucky-mac11
+ * Last Updated: Oct 2, 2017
+ * Controls basic user input and how it affects the library
+ */
+package Controllers;
+import java.util.*;
+import Models.*;
+import Views.HelpView;;
+
+public class LibraryController {
+	private Scanner scan = new Scanner(System.in);
+	private Library myLibrary;
+	private Boolean keepGoing;
+	
+	/* LibraryController Constructor
+	 * Creates a library to be searched or manipulated
+	 */
+	public LibraryController(){
+		myLibrary = new Library();	// Library object, the item that is manipulated
+	}
+	
+	/* Main controller for library, runs a loop while 
+	 * taking user input for how to manipulate the
+	 * library.
+	 */
+	public void LibraryView(){
+		// Boolean object to check if user requested to quit
+		keepGoing = true;
+		while(keepGoing){
+			System.out.println("Hello, what would you like to do?");
+			
+			// User response, split at every '--' 
+			String[] response = scan.nextLine().split("--");
+			
+			if(response[0].equalsIgnoreCase("Help")){
+				// Calls HelpView object to output the options menu
+				new HelpView();
+				
+			} else if(response[0].equalsIgnoreCase("Quit")){
+				/* Set loop Boolean to false so the 
+				 * program can quit
+				 */
+				keepGoing = false;
+				
+			} else if(response[0].equalsIgnoreCase("Print")){
+				// Outputs the movie and its contents
+				printMovie();
+				
+			} else if (response[0].equalsIgnoreCase("Add")){
+				// Adds a movie to the library
+				if(response.length == 3){
+					// Add a movie with a rating
+					myLibrary.addMovie(response[1],response[2]);
+				} else if (response.length == 2){
+					// Add a movie with just the title
+					myLibrary.addMovie(response[1], null);
+				} else {
+					// If no movie is entered, ask for a movie
+					System.out.println("Please enter a movie title to add.");
+					myLibrary.addMovie(scan.nextLine(),null);
+				}
+				
+			}  else if (response[0].equalsIgnoreCase("Search")){
+				// Search for, and output movie details
+				Movie movie = search(response[0]); 
+				if(movie != null){
+					System.out.println(movie.toString());
+				}
+				
+			} else if((response[0].equalsIgnoreCase("Edit"))&&(response.length>1)){
+				Movie movie = search(response[1]);
+				if(movie != null){
+					if(response.length != 4){
+						System.out.println("Please enter a property to change");
+						response[0] = scan.nextLine();
+						
+						System.out.println("Please enter the new value");
+						response[1] = scan.nextLine();
+						edit(movie, response[0], response[1]);
+					} else {
+						edit(movie, response[2], response[3]);
+					}
+					System.out.println(movie.toString());
+				}
+			} else {
+				System.out.println("Error - invalid command");
+				System.out.println("Type 'Help' for assistance.");
+			}
+			
+		}
+		
+		System.out.println("Goodbye");
+		scan.close();
+	}
+	
+	private void printMovie(){
+		if(myLibrary.getSize() == 0){
+			System.out.println("You have no movies");
+		} else {
+			myLibrary.printLibrary();
+		}
+	}
+	
+	private Movie search(String name){
+		Movie wanted = myLibrary.searchLibrary(name);
+		if(wanted == null){
+			System.out.println("The movie does not exist in your library");
+		}
+		
+		return wanted;
+	}
+
+	private void edit(Movie movie, String property, String newValue){
+		if(property.equalsIgnoreCase("Name")){
+			movie.setName(newValue);
+		} else if(property.equalsIgnoreCase("Rating")){
+			movie.setRating(newValue);
+		} else if(property.equalsIgnoreCase("Format")){
+			movie.setFormat(newValue);
+		} else if(property.equalsIgnoreCase("Year")){
+			movie.setYear(newValue);
+		}
+	}
+}
--- MovieDatabase/src/MovieLibrary.java
+++ MovieDatabase/src/MovieLibrary.java
@@ -2,12 +2,13 @@
  * Last Updated: Oct 2, 2017
  * Starts the movie library
  */
+import Controllers.LibraryController;
 
 public class MovieLibrary {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		LibraryViewer mine = new LibraryViewer();
+		LibraryController mine = new LibraryController();
 		mine.LibraryView();
 	}
 
